47acd4b4d465a8242780f6eb54a48ff5be3ccf65
Marco Mascorro <75904844+mamascor@users.noreply.github.com>
2022-06-04 11:47:05 -0500

    Initial Commit

---
+++ Next ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Next ToDo
+//
+//  Created by Marco Mascorro on 6/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
